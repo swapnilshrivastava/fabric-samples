27c5a019e213ebc2a30cb16a02468ffa477883de
Swapnil Shrivastava <shrivastava_sw@India.XoriantCorp.com>
2018-03-27 12:50:42 +0530

    bug fix

--- balance-transfer/artifacts/src/github.com/loanApp/loanApp.go
+++ balance-transfer/artifacts/src/github.com/loanApp/loanApp.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-    "fmt"
 	"encoding/json"
-    "github.com/hyperledger/fabric/core/chaincode/shim"
-    "github.com/hyperledger/fabric/protos/peer"
+	"fmt"
+	"strconv"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"github.com/hyperledger/fabric/protos/peer"
 )
 
 // SimpleAsset implements a simple chaincode to manage an asset
@@ -14,15 +16,15 @@ type SimpleAsset struct {
 type loanApplication struct {
 	ID string `json:"id"`
 	//UserId user `json:"id"`
-	Status string `json:"status"`
+	Status          string `json:"status"`
 	RequestedAmount string `json:"requestedAmount"`
-	ProcessedBy string `json:"processedby"`
-   }
+	ProcessedBy     string `json:"processedby"`
+}
 
 // Init is called during chaincode instantiation to initialize any
 // data. Note that chaincode upgrade also calls this function to reset
 // or to migrate data.
-func (t *SimpleAsset) Init(stub shim.ChaincodeStubInterface) peer.Response {
+/*func (t *SimpleAsset) Init(stub shim.ChaincodeStubInterface) peer.Response {
     // Get the args from the transaction proposal
     args := stub.GetStringArgs()
     if len(args) != 2 {
@@ -37,72 +39,108 @@ func (t *SimpleAsset) Init(stub shim.ChaincodeStubInterface) peer.Response {
             return shim.Error(fmt.Sprintf("Failed to create asset: %s", args[0]))
     }
     return shim.Success(nil)
+}*/
+
+func (t *SimpleAsset) Init(stub shim.ChaincodeStubInterface) pb.Response {
+	logger.Info("########### example_cc0 Init ###########")
+
+	_, args := stub.GetFunctionAndParameters()
+	var A, B string    // Entities
+	var Aval, Bval int // Asset holdings
+	var err error
+
+	// Initialize the chaincode
+	A = args[0]
+	Aval, err = strconv.Atoi(args[1])
+	if err != nil {
+		return shim.Error("Expecting integer value for asset holding")
+	}
+	B = args[2]
+	Bval, err = strconv.Atoi(args[3])
+	if err != nil {
+		return shim.Error("Expecting integer value for asset holding")
+	}
+	logger.Info("Aval = %d, Bval = %d\n", Aval, Bval)
+
+	// Write the state to the ledger
+	err = stub.PutState(A, []byte(strconv.Itoa(Aval)))
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	err = stub.PutState(B, []byte(strconv.Itoa(Bval)))
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	return shim.Success(nil)
+
 }
 
 // Invoke is called per transaction on the chaincode. Each transaction is
 // either a 'get' or a 'set' on the asset created by Init function. The Set
 // method may create a new asset by specifying a new key-value pair.
 func (t *SimpleAsset) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
-    // Extract the function and args from the transaction proposal
-    fn, args := stub.GetFunctionAndParameters()
+	// Extract the function and args from the transaction proposal
+	fn, args := stub.GetFunctionAndParameters()
 
-    var result string
-    var err error
-    if fn == "set" {
-            result, err = set(stub, args)
-    } else { // assume 'get' even if fn is nil
-            result, err = get(stub, args)
-    }
-    if err != nil {
-            return shim.Error(err.Error())
-    }
+	var result string
+	var err error
+	if fn == "set" {
+		result, err = set(stub, args)
+	} else { // assume 'get' even if fn is nil
+		result, err = get(stub, args)
+	}
+	if err != nil {
+		return shim.Error(err.Error())
+	}
 
-    // Return the result as success payload
-    return shim.Success([]byte(result))
+	// Return the result as success payload
+	return shim.Success([]byte(result))
 }
 
 // Set stores the asset (both key and value) on the ledger. If the key exists,
 // it will override the value with the new one
 func set(stub shim.ChaincodeStubInterface, args []string) (string, error) {
-    if len(args) != 2 {
-            return "", fmt.Errorf("Incorrect arguments. Expecting a key and a value")
-    }
+	if len(args) != 2 {
+		return "", fmt.Errorf("Incorrect arguments. Expecting a key and a value")
+	}
 	var loanApplicationId = args[0]
 	var loanApplicationInput = args[1]
 	ID := "123"
 	Status := "123"
 	RequestedAmount := "100000"
 	ProcessedBy := "456"
-	
+
 	loanApplication := &loanApplication{ID, Status, RequestedAmount, ProcessedBy}
 	loanApplicationJSONasBytes, err := json.Marshal(loanApplication)
-	
-    err = stub.PutState(loanApplicationId, []byte(loanApplicationJSONasBytes))
-    if err != nil {
-            return "", fmt.Errorf("Failed to set asset: %s", args[0])
-    }
-    return loanApplicationInput, nil
+
+	err = stub.PutState(loanApplicationId, []byte(loanApplicationJSONasBytes))
+	if err != nil {
+		return "", fmt.Errorf("Failed to set asset: %s", args[0])
+	}
+	return loanApplicationInput, nil
 }
 
 // Get returns the value of the specified asset key
 func get(stub shim.ChaincodeStubInterface, args []string) (string, error) {
-    if len(args) != 1 {
-            return "", fmt.Errorf("Incorrect arguments. Expecting a key")
-    }
+	if len(args) != 1 {
+		return "", fmt.Errorf("Incorrect arguments. Expecting a key")
+	}
 
-    value, err := stub.GetState(args[0])
-    if err != nil {
-            return "", fmt.Errorf("Failed to get asset: %s with error: %s", args[0], err)
-    }
-    if value == nil {
-            return "", fmt.Errorf("Asset not found: %s", args[0])
-    }
-    return string(value), nil
+	value, err := stub.GetState(args[0])
+	if err != nil {
+		return "", fmt.Errorf("Failed to get asset: %s with error: %s", args[0], err)
+	}
+	if value == nil {
+		return "", fmt.Errorf("Asset not found: %s", args[0])
+	}
+	return string(value), nil
 }
 
 // main function starts up the chaincode in the container during instantiate
 func main() {
-    if err := shim.Start(new(SimpleAsset)); err != nil {
-            fmt.Printf("Error starting SimpleAsset chaincode: %s", err)
-    }
+	if err := shim.Start(new(SimpleAsset)); err != nil {
+		fmt.Printf("Error starting SimpleAsset chaincode: %s", err)
+	}
 }
